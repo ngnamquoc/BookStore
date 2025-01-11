f42d15d2d0901f9c35478dd9fd4a4e8d4fd36120
Nam Quoc <nguyennamquoc.mar@gmail.com>
2025-01-11 10:44:48 -0500

    configure database and add categories table

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        //categories table
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,7 +1,12 @@
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//add entity framework core with sql server
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -11,6 +16,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=index}/{id?}"
+);
 
 app.Run();
